fd8661b257eb115c7d307143d97456b6a1ada798
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-06 21:42:25 +0300

    [:main] Impl | Example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#define OHDEBUG_ENABLE_ALL_BY_DEFAULT 0  // Only print those groups which we explicitly allow. Remove it to restore to the original configuration (allow all by default)
 #include "OhDebug.hpp"
+
+ohdebuggroup(1)  // Explicitly allow printing group 1 (or disallow, if OHDEBUG_ENABLE_ALL_BY_DEFAULT=1)
 
 int main(void)
 {
-	return 0;
+	int a = 0;
+	int b = 1;
+	ohdebug(0, 777, a, b);  // ...
+	ohdebug(1, 42, a, b);  // 42=42  a=0  b=1
 }
