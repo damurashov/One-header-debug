b5fcf69ad950e6e6663b1c33c59d775d37f9f005
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-08 13:55:14 +0300

    [tools:ohdebuggen] Enh | Output to file

--- tools/ohdebuggen.py
+++ tools/ohdebuggen.py
@@ -2,17 +2,26 @@
 
 ARG_NUM = 20
 
-def main():
+def gen():
 	for i in range(1, ARG_NUM):
-		print("""\
+		yield """\
 # define ohdebug%d__(context, a, ...) OhDebug::ohdebugImpl<static_cast<unsigned>(context)>(#a, a); \\
-	ohdebug%d__(static_cast<unsigned>(context), ## __VA_ARGS__);""" % (i, i + 1))
+	ohdebug%d__(static_cast<unsigned>(context), ## __VA_ARGS__);
+""" % (i, i + 1)
 
-	print("""\
+	yield """\
 # define ohdebug%d__(context, a, ...) OhDebug::ohdebugImpl<static_cast<unsigned>(context)>(#a, a); \\
-	ohdebugend__(static_cast<unsigned>(context), ## __VA_ARGS__);""" % (ARG_NUM))
+	ohdebugend__(static_cast<unsigned>(context), ## __VA_ARGS__);
+""" % (ARG_NUM)
 
-	print(f" # define ohdebug(context, ...) ohdebug0__(context, ##__VA_ARGS__, %s)" % ', '.join(["OhDebug::Stub{}"] * ARG_NUM))
+	yield f" # define ohdebug(context, ...) ohdebug0__(context, ##__VA_ARGS__, %s)\n" % ', '.join(["OhDebug::Stub{}"] * ARG_NUM)
+
+
+def main():
+	with open("generated", 'w') as f:
+		for g in gen():
+			f.write(g)
+
 
 if __name__ == "__main__":
 	main()
