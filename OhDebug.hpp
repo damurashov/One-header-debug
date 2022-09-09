afd31f00d18ab649a6bb7f3f2f8d597a82b1a4f2
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-09 10:16:39 +0300

    [:OhDebug] Ref | Naming for macros

--- OhDebug.hpp
+++ OhDebug.hpp
@@ -174,11 +174,12 @@ void ohDebugPrintNl()
 }  // namespace OhDebug
 
 # define ohdebugstringify__(a) #a
-# define ohdebugflimpl(line) __FILE__ ":" #line
-# define ohdebugfl(line) ohdebugflimpl(line)
+# define ohdebugflimpl__(line) __FILE__ ":" #line
+# define ohdebugfl__(line) ohdebugflimpl__(line)
 # define ohdebugvoid(a) (a, "")
 
-# define ohdebug0__(context, a, ...) OhDebug::ohDebugPrintGroup<OHDEBUG_COMPILE_TIME_CRC32_STR(#context)>(ohdebugfl(__LINE__)); \
+# define ohdebug0__(context, a, ...) \
+	OhDebug::ohDebugPrintGroup<OHDEBUG_COMPILE_TIME_CRC32_STR(#context)>(ohdebugfl__(__LINE__)); \
 	OhDebug::ohDebugPrintGroup<OHDEBUG_COMPILE_TIME_CRC32_STR(#context)>(#context); \
 	ohdebug1__(OHDEBUG_COMPILE_TIME_CRC32_STR(#context), a, ## __VA_ARGS__)
 
@@ -288,7 +289,7 @@ void ohDebugPrintNl()
 	OhDebug::ohDebugPrintNl<static_cast<unsigned>(context)>()
 
 # define ohdebugstr(context, a) \
-	OhDebug::ohDebugPrintGroup<OHDEBUG_COMPILE_TIME_CRC32_STR(#context)>(ohdebugfl(__LINE__)); \
+	OhDebug::ohDebugPrintGroup<OHDEBUG_COMPILE_TIME_CRC32_STR(#context)>(ohdebugfl__(__LINE__)); \
 	OhDebug::ohDebugPrintGroup<OHDEBUG_COMPILE_TIME_CRC32_STR(#context)>(#context); \
 	std::cout << ((void)a, #a) << std::endl;
 
