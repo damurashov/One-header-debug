af222b8019384240ff45fa9364e17f1e0f150b0d
DmitryMurashov <dmtr.murashov@gmail.com>
2023-03-31 12:50:07 +0300

    [:main] Enh | More versatile example

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include "OhDebug.hpp"
 
 OHDEBUG_TAG_ENABLE("Echo")
+// OHDEBUG_TAG_ENABLE("Echo2")
 
 int main(void)
 {
 	OHDEBUG("Echo", 1, "some string", 42U);
+	OHDEBUG("Echo2", 1, "some string", 42U);
 }
