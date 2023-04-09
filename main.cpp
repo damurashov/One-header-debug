ec6b1a44fb4f606cf1e148bef5c9ce086e1a5f16
DmitryMurashov <dmtr.murashov@gmail.com>
2023-04-09 18:53:53 +0300

    [:main] Enh | More explicit example

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #define OHDEBUG_TAGS_ENABLE "Echo2", "Echo3"
 
 #include "OhDebug.hpp"
-
-OHDEBUG_TAG_ENABLE("Echo")
-// OHDEBUG_TAG_ENABLE("Echo2")
+#include <cassert>
+#include <cstring>
 
 OHDEBUG_TEST("Some test")
 {
+	OHDEBUG("Echo1", "This will not be printed");
 	OHDEBUG("Echo2", "This is echo");
+	assert(strcmp("Someone who uses OHDEBUG", "Someone who needs a wrapper over standard C assert") != 0);
+	assert(strcmp("Do not forget to compile it with -g flag",
+		"So it will show the exact place of where an assert has been triggered") != 0);
+	// assert(strcmp("Keep it simple", "Continue tests after an assert is triggered") == 0);
 }
 
-OHDEBUG_TEST("Some test")
+OHDEBUG_TEST("Some other test")
 {
 	OHDEBUG("Echo2", "This is echo");
 }
