eb6f95cb056d6694c7b3a192adebebca5f5e3ab0
DmitryMurashov <dmtr.murashov@gmail.com>
2023-04-09 18:30:52 +0300

    [:main] Impl | Added tests

--- main.cpp
+++ main.cpp
@@ -6,9 +6,20 @@
 OHDEBUG_TAG_ENABLE("Echo")
 // OHDEBUG_TAG_ENABLE("Echo2")
 
+OHDEBUG_TEST("Some test")
+{
+	OHDEBUG("Echo2", "This is echo");
+}
+
+OHDEBUG_TEST("Some test")
+{
+	OHDEBUG("Echo2", "This is echo");
+}
+
 int main(void)
 {
 	OHDEBUG("Echo", 1, "some string", 42U);
 	OHDEBUG("Echo2", 1, "some string", 42U);
 	OHDEBUG("Echo3", 1, "some string", 42U);
+	OHDEBUG_RUN_TESTS();
 }
